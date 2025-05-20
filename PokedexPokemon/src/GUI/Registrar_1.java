449999cc9536021d8944b90b3f184ccff9c41d8e
oscar <oscar@Oscar>
2025-05-19 20:32:01 -0600

    Agregue una imagen faltante

--- PokedexPokemon/src/GUI/Registrar_1.java
+++ PokedexPokemon/src/GUI/Registrar_1.java
@@ -72,6 +72,7 @@ public class Registrar_1 extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(255, 204, 0));
         jLabel1.setText("Regsitrar Pokemones");
 
+        btnRegresarPokedex.setIcon(new javax.swing.ImageIcon("C:\\Users\\oscar\\Downloads\\izquierda.png")); // NOI18N
         btnRegresarPokedex.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnRegresarPokedexActionPerformed(evt);
